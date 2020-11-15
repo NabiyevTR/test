a820028cdcddd99400c6317d178c0294136ca72d
NabiyevTR <NabiyeTR@gmail.com>
2020-11-15 21:24:24 +0300

    Add template

---
+++ desktop/src/ru/ntr/study/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.ntr.study.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.ntr.study.testProject;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new testProject(), config);
+	}
+}
